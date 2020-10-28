ee02b3d462ebb52ff9f03edd3b2085dc8c9e7582
pattarawan26034543 <pattarawan26034543@gmail.com>
2020-10-28 16:51:16 +0700

    สอบปลายภาค

---
+++ register_app/src/views/About.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-container>
+     เกี่ยวกับเรา
+  </v-container>
+</template>
+<script>
+export default {
+}
+</script>
+<style>
+</style>
---
+++ register_app/src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-container>
+     <v-row>
+       <v-col cols="4">
+<v-card color="#385F73" dark>
+            <v-card-title class="headline">หน้าแรก</v-card-title>
+            <v-card-actions>
+              <v-btn text></v-btn>
+            </v-card-actions>
+          </v-card>
+       </v-col>
+       <v-col cols="4">
+<v-card color="#385F73" dark>
+            <v-card-text ><div class="font-big">การบูรณาการการเขียนโปรแกรม</div></v-card-text>
+            <v-card-actions>
+              <v-btn text></v-btn>
+            </v-card-actions>
+          </v-card>
+       </v-col>
+     </v-row>
+  </v-container>
+</template>
+<script>
+export default {
+}
+</script>
+<style scope>
+.font-big{
+
+ font-size: 20px;
+}
+</style>
